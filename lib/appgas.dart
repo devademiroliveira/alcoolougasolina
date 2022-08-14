cf56b53c2e4059bdd9f0adfd22c2af7134345b95
Oliveira Ademir <aoc.ade.costa@gmail.com>
2022-08-14 19:32:29 -0300

    att

--- lib/appgas.dart
+++ lib/appgas.dart
@@ -21,7 +21,7 @@ class _AppGasState extends State<AppGas> {
     if (precoAlcool == null || precoGasolina == null) {
       setState(() {
         _mensagem =
-            'OPS! Número inválido, apenas valores maior que 0 e separados por , ou .';
+            'OPS! Número inválido, apenas valores maiores que 0 e separados por , ou .';
       });
     } else {
       if (precoAlcool / precoGasolina >= 0.7) {
@@ -51,73 +51,82 @@ class _AppGasState extends State<AppGas> {
           child: Text('Álcool ou Gasolina'),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Padding(
-                padding: const EdgeInsets.only(bottom: 16.0),
-                child: Image.asset('image/alcxgas.jpg')),
-            const Text(
-              'Qual combustível é melhor para você!',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller:
-                    _controllerAlcool, //responsável por capturar o texto digitado
-                keyboardType: TextInputType.number,
-                onSubmitted: (String alcool) {},
-                decoration: const InputDecoration(
-                  labelStyle: TextStyle(
-                    fontSize: 26,
-                    inherit: true,
-                  ),
-                  labelText: 'Preço Álcool, ex: 4,15',
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: _controllerGasolina,
-                //responsável por capturar o texto digitado
-                keyboardType: TextInputType.number,
-                onSubmitted: (String gasolina) {},
-                decoration: const InputDecoration(
-                  labelStyle: TextStyle(
-                    fontSize: 26,
-                    inherit: true,
-                  ),
-                  labelText: 'Preço Gasolina, ex: 4,15',
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                onPressed: _calcular,
-                child: const Text(
-                  'Calcular',
+      body: Padding(
+        padding: const EdgeInsets.only(bottom: 8),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Image.asset(
+                    'image/alcxgas.jpg',
+                    height: 270,
+                  )),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Qual combustível é melhor para você!',
                   style: TextStyle(
-                    fontSize: 20,
                     fontWeight: FontWeight.bold,
+                    fontSize: 20,
                   ),
                 ),
               ),
-            ),
-            Text(
-              _mensagem,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller:
+                      _controllerAlcool, //responsável por capturar o texto digitado
+                  keyboardType: TextInputType.number,
+                  onSubmitted: (String alcool) {},
+                  decoration: const InputDecoration(
+                    labelStyle: TextStyle(
+                      fontSize: 26,
+                      inherit: true,
+                    ),
+                    labelText: 'Preço Álcool, ex: 4,15',
+                  ),
+                ),
               ),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _controllerGasolina,
+                  //responsável por capturar o texto digitado
+                  keyboardType: TextInputType.number,
+                  onSubmitted: (String gasolina) {},
+                  decoration: const InputDecoration(
+                    labelStyle: TextStyle(
+                      fontSize: 26,
+                      inherit: true,
+                    ),
+                    labelText: 'Preço Gasolina, ex: 4,15',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  onPressed: _calcular,
+                  child: const Text(
+                    'Calcular',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Text(
+                _mensagem,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
